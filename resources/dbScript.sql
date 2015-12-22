e7c715439f006e52a9e70ebdfd133902536c5e7e
sunny <sunny.mishra@thoughtfocus.com>
2015-12-22 20:37:58 +0530

    DBscript updated

--- resources/dbScript.sql
+++ resources/dbScript.sql
@@ -1,5 +1,26 @@
 
+
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `f_friend_request_type`
+-- ----------------------------
+DROP TABLE IF EXISTS `f_friend_request_type`;
+CREATE TABLE `f_friend_request_type` (
+  `friendRequestTypeId` int(5) NOT NULL,
+  `friendRequestTypeName` varchar(100) DEFAULT NULL,
+  `active` bit(1) NOT NULL,
+  PRIMARY KEY (`friendRequestTypeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of f_friend_request_type
+-- ----------------------------
+INSERT INTO `f_friend_request_type` VALUES ('1', 'PENDING_ACCEPTANCE', '');
+INSERT INTO `f_friend_request_type` VALUES ('2', 'PENDING_APPROVAL', '');
+INSERT INTO `f_friend_request_type` VALUES ('3', 'CONFIRMED', '');
+INSERT INTO `f_friend_request_type` VALUES ('4', 'REVOKED', '');
+INSERT INTO `f_friend_request_type` VALUES ('5', 'REJECTED', '');
 
 -- ----------------------------
 -- Table structure for `f_goal`
@@ -14,7 +35,7 @@ CREATE TABLE `f_goal` (
   `goalTargetValue` int(11) DEFAULT NULL,
   `goalUnit` varchar(12) DEFAULT NULL,
   `isPrivateGoal` bit(1) DEFAULT b'0',
-  `goalMembersCount` int(10) NOT NULL DEFAULT 1,
+  `goalMembersCount` int(10) NOT NULL DEFAULT '1',
   `createdDate` date DEFAULT NULL,
   `createdBy` bigint(20) DEFAULT NULL,
   `modifiedDate` date DEFAULT NULL,
@@ -25,14 +46,17 @@ CREATE TABLE `f_goal` (
   KEY `goalTypeId` (`goalTypeId`),
   CONSTRAINT `f_goal_ibfk_1` FOREIGN KEY (`tagId`) REFERENCES `f_tag` (`tagId`),
   CONSTRAINT `f_goal_ibfk_2` FOREIGN KEY (`goalTypeId`) REFERENCES `f_goal_type` (`goalTypeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of f_goal
 -- ----------------------------
-INSERT INTO `f_goal` VALUES ('1', '1', '1', 'Lose 10 KG', 'Need to lose 10 KG weight in 1 month period', '10', 'KG', '2015-11-10', '1', null, null, '');
-INSERT INTO `f_goal` VALUES ('2', '1', '1', 'Run 1KM each day', '', '1', 'KM', '2015-11-10', '1', null, null, '');
-INSERT INTO `f_goal` VALUES ('3', '2', '1', 'Finish Harry Potter last book', 'Complete 500  pages of Harry potter\'s last novel', '500', 'No', '2015-11-10', '1', null, null, '');
+INSERT INTO `f_goal` VALUES ('1', '1', '1', 'Lose 10 KG', 'Need to lose 10 KG weight in 1 month period', '10', 'KG', '', '1', '2015-11-10', '1', null, null, '');
+INSERT INTO `f_goal` VALUES ('2', '1', '1', 'Run 1KM each day', '', '1', 'KM', '', '1', '2015-11-10', '1', null, null, '');
+INSERT INTO `f_goal` VALUES ('3', '2', '1', 'Finish Harry Potter last book', 'Complete 500  pages of Harry potter\'s last novel', '500', 'No', '', '1', '2015-11-10', '1', null, null, '');
+INSERT INTO `f_goal` VALUES ('6', '1', '1', 'Fitness Example', 'This is a fitness example goal', '21', 'kg', '', '1', '2015-12-01', '1', null, null, '');
+INSERT INTO `f_goal` VALUES ('11', '1', '1', 'Harry potter book', '', '500', 'Pages', '', '1', '2015-12-18', '12', null, null, '');
+INSERT INTO `f_goal` VALUES ('12', '1', '1', 'Run 1KM each day', '', '1', 'KM', '', '1', '2015-12-19', '1', null, null, '');
 
 -- ----------------------------
 -- Table structure for `f_goal_tracker`
@@ -94,6 +118,7 @@ CREATE TABLE `f_goal_type` (
 -- ----------------------------
 INSERT INTO `f_goal_type` VALUES ('1', 'Target', '');
 INSERT INTO `f_goal_type` VALUES ('2', 'Habit', '');
+INSERT INTO `f_goal_type` VALUES ('5', 'JOIN_REQUEST_REJECTED', '');
 
 -- ----------------------------
 -- Table structure for `f_tag`
@@ -128,9 +153,9 @@ CREATE TABLE `f_user` (
   `phone` varchar(20) DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
   `zipCode` varchar(10) DEFAULT NULL,
-  `goldCoins` int(11) DEFAULT 0;
-  `silverCoins` int(11) DEFAULT 0;
-  `aboutMe` varchar(150) DEFAULT NULL;
+  `aboutMe` varchar(150) DEFAULT NULL,
+  `silverCoins` int(11) DEFAULT '0',
+  `goldCoins` int(11) DEFAULT '0',
   `password` varchar(200) DEFAULT NULL,
   `forgotPasswordCode` varchar(200) DEFAULT NULL,
   `isPasswordVerified` bit(1) DEFAULT NULL,
@@ -138,13 +163,16 @@ CREATE TABLE `f_user` (
   `modifiedDate` date DEFAULT NULL,
   `active` bit(1) NOT NULL,
   PRIMARY KEY (`userId`),
-  UNIQUE KEY (userName, email)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `constr_user` (`displayName`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of f_user
 -- ----------------------------
-INSERT INTO `f_user` VALUES ('1', 'Sunny', null, 'sunny@sunny.com', null, null, null, null, 'f04ba60a118ea37608c8f515677a1be9284708ebca64e095a446ed7a4d772abd', null, null, '2015-11-01', null, '');
+INSERT INTO `f_user` VALUES ('1', 'Sunny', null, 'sunny@sunny.com', null, null, null, null, '0', '0', 'f04ba60a118ea37608c8f515677a1be9284708ebca64e095a446ed7a4d772abd', null, null, '2015-11-01', null, '');
+INSERT INTO `f_user` VALUES ('6', 'sunny@sunny.com', null, 'sunny@sunny.com', '9999999', null, null, null, '0', '0', 'f04ba60a118ea37608c8f515677a1be9284708ebca64e095a446ed7a4d772abd', null, null, '2015-11-18', null, '');
+INSERT INTO `f_user` VALUES ('12', 'Sunny1', null, 'sunny1@sunny.com', null, null, null, null, '0', '0', '2666be9d3df17f4b242f5fd0d9b929b64693faa8ef149eedb9fd5ebabd564021', null, null, '2015-11-22', null, '');
+INSERT INTO `f_user` VALUES ('13', 'Sunny12', null, 'sunny12@sunny.com', null, null, null, null, '0', '0', '7a57953a3ed902b3da695f57d03518f456d9162cdb9dbb7b5fb72c41bb841bec', null, null, '2015-11-22', null, '');
 
 -- ----------------------------
 -- Table structure for `f_user_goal`
@@ -164,44 +192,21 @@ CREATE TABLE `f_user_goal` (
   `createdBy` int(11) DEFAULT NULL,
   `modifiedDate` date DEFAULT NULL,
   `modifiedBy` date DEFAULT NULL,
-  `statusType` int(5) NOT NULL,
+  `statusType` int(5) DEFAULT NULL,
   PRIMARY KEY (`userGoalId`),
+  UNIQUE KEY `userId_2` (`userId`,`goalId`),
   KEY `userId` (`userId`),
   KEY `goalId` (`goalId`),
-  UNIQUE KEY (`userId`, `goalId`),
   CONSTRAINT `f_user_goal_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `f_user` (`userId`),
   CONSTRAINT `f_user_goal_ibfk_2` FOREIGN KEY (`goalId`) REFERENCES `f_goal` (`goalId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of f_user_goal
 -- ----------------------------
-INSERT INTO `f_user_goal` VALUES ('1', '1', '1', '90', '2015-10-18', '2015-11-18', null, null, '', '', '2015-11-10', null, '2015-11-11', null, '');
-INSERT INTO `f_user_goal` VALUES ('2', '1', '2', null, '2015-10-20', '2015-12-20', null, null, '', '', '2015-11-10', null, null, null, '');
-INSERT INTO `f_user_goal` VALUES ('3', '1', '3', null, '2015-10-11', '2015-12-16', null, null, '', '', '2015-11-10', null, null, null, '');
-
--- ----------------------------
--- Table structure for `f_friend_request_type`
--- ----------------------------
-CREATE TABLE `f_friend_request_type` (
-  `friendRequestTypeId` int(5) NOT NULL,
-  `friendRequestTypeName` varchar(100) DEFAULT NULL,
-  `active` bit(1) NOT NULL,
-  PRIMARY KEY (`friendRequestTypeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of f_friend_request_type
--- ----------------------------
-INSERT INTO `f_friend_request_type` VALUES (1, 'SHARED_PENDING', 1);
-INSERT INTO `f_friend_request_type` VALUES (2, 'PENDING	', 1);
-INSERT INTO `f_friend_request_type` VALUES (3, 'CONFIRMED', 1);
-INSERT INTO `f_friend_request_type` VALUES (4, 'REVOKED', 1);
-INSERT INTO `f_friend_request_type` VALUES (5, 'REJECTED', 1);
-
-
-
-
-
-
-
+INSERT INTO `f_user_goal` VALUES ('1', '1', '1', '90', '2015-10-18', '2015-11-18', null, null, '', '2015-11-10', null, '2015-11-11', null, '3');
+INSERT INTO `f_user_goal` VALUES ('2', '1', '2', '50', '2015-10-20', '2015-12-20', null, null, '', '2015-11-10', null, null, null, '3');
+INSERT INTO `f_user_goal` VALUES ('3', '1', '3', '10', '2015-10-11', '2015-12-16', null, null, '', '2015-11-10', null, null, null, '3');
+INSERT INTO `f_user_goal` VALUES ('10', '12', '1', null, '2015-10-20', '2015-12-20', null, null, '', '2015-12-16', '12', null, null, '3');
+INSERT INTO `f_user_goal` VALUES ('12', '12', '11', null, '2015-10-20', '2015-12-20', null, null, '', '2015-12-18', null, null, null, '3');
+INSERT INTO `f_user_goal` VALUES ('13', '1', '12', null, '2015-10-20', '2015-12-20', null, null, '', '2015-12-19', null, null, null, '3');
